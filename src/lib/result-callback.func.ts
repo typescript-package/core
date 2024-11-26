0bd7d64b5a0de363ea27c10463754f68225e4073
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2024-11-26 22:35:01 +0000

    docs(resultCallback): update.

--- src/lib/result-callback.func.ts
+++ src/lib/result-callback.func.ts
@@ -1,6 +1,6 @@
 /**
- * Default function to handle `callback` of functions.
- * @param result A value of `boolean` type of the result of the check.
- * @returns The return value is a `boolean` of the result of the check.
+ * @description Default function to handle `callback` of functions.
+ * @param {boolean} result - A value of `boolean` type of the result of the check.
+ * @returns {boolean} The returned value is a `boolean` of the result of the check.
  */
 export const resultCallback = (result: boolean): boolean => result;
